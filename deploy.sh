b816cee2b46a11d9f86b316c1ef4ff1cd7848ea0
astle286 <astle257@gmail.com>
2026-02-03 15:31:43 +0530

    ec2 deploy-08

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 set -e
 
 echo ">>> Updating project files..."
-cd ~/devops-helper
+
 echo ">>> Logging in to GHCR..."
-echo $GHCR_PAT | docker login ghcr.io -u YOUR_GITHUB_USERNAME --password-stdin
+echo $GHCR_PAT | docker login ghcr.io -u astle286 --password-stdin
 
 echo ">>> Pulling latest image..."
-docker pull ghcr.io/YOUR_GITHUB_USERNAME/devops-helper:latest
+docker pull ghcr.io/astle286/devops-helper:latest
 
 echo ">>> Restarting containers..."
-docker-compose down
+
 docker-compose up -d
 
 echo ">>> Deployment complete! Running latest"
