50eca150d852c6ac35c5088a0d0c2f609d47c50a
astle286 <astle257@gmail.com>
2026-02-03 15:55:35 +0530

    ec2 deploy-11

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 echo ">>> Updating project files..."
 
 echo ">>> Logging in to GHCR..."
-echo $GHCR_PAT | docker login ghcr.io -u astle286 --password-stdin
+echo "$GHCR_PAT" | docker login ghcr.io -u astle286 --password-stdin
 
 echo ">>> Pulling latest image..."
 docker pull ghcr.io/astle286/devops-helper:latest
